461e06628c550f0db4a9bf80af161dceae20ee50
lookup57lookup57-bit <lookup57lookup57@gmail.com>
2026-01-22 23:09:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
 from fastapi import FastAPI
-from db import Base, engine
 
 app = FastAPI()
 
-@app.on_event("startup")
-def start():
-    Base.metadata.create_all(engine)
-
 @app.get("/")
-def root():
-    return {"status": "Backend running"}
+def home():
+    return {"status": "API running successfully"}
